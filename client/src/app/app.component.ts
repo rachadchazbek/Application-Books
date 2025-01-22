c6697757bca7a7ce2821fd9498a2191dd55daad6
rachadchazbek17 <rachadchazbek@gmail.com>
2025-01-22 14:17:33 -0500

    Migration to angular 19 completed succesfully ater making all components standalone

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router';
+import { Router, NavigationStart, RouterModule } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- client/src/app/main-page/main-page.component.ts
+++ client/src/app/main-page/main-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
+  standalone: true,
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
