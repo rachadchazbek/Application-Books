73587fe514137120fad90eb171d4f8d86244969b
rachadchazbek17 <rachadchazbek@gmail.com>
2025-01-28 12:32:18 -0500

    cleanup code

--- server/app/index.ts
+++
@@ -1,7 +0,0 @@
-import 'reflect-metadata';
-import 'module-alias/register';
-import { Server } from '@app/server';
-import { Container } from 'typedi';
-
-const server: Server = Container.get(Server);
-server.init();
