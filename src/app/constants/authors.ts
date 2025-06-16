071b570af357b88ef3b5668248611e8b2cb30fc9
rachadchazbek17 <rachadchazbek@gmail.com>
2025-06-16 08:20:58 +0300

    deleted authors.json

--- src/app/constants/authors.ts
+++ src/app/constants/authors.ts
@@ -4,5 +4,5 @@ import authors from './authors.json';
  * Array of all author names extracted from the authors.json file
  */
 export const AUTHOR_NAMES: string[] = authors.results.bindings.map(
-  (binding) => binding.authorName.value
+  (binding) => binding.preferredAuthorName.value
 );
